df704c8b78efb1dfda91aa7bce1a2f3651362c88
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2023-12-18 13:43:27 -0600

    reinstated starter code

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,5 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const mysql = require('mysql');
-
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  username: 'root',
-  password: 'atkababy',
-  database: 'ecommerce_db'
-});
-
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database:', err);
-    return;
-  }
-  console.log('Connected to database');
-  startApp();
-});
 
 router.use('/api', apiRoutes);
 
