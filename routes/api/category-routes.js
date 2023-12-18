05f7f124a14484cd2827195b45750d9d299bff7b
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2023-12-18 14:14:07 -0600

    altered routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,88 +1,63 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
-// GET all categories with associated Products
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.findAll({
-      include: [Product],
+      include: [{ model: Product }],
     });
-    res.json(categories);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve categories' });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-// GET a single category by its `id` with associated Products
 router.get('/:id', async (req, res) => {
   try {
     const category = await Category.findByPk(req.params.id, {
-      include: [Product],
+      include: [{ model: Product }],
     });
-
     if (!category) {
-      return res.status(404).json({ error: 'Category not found' });
+      res.status(404).json({ message: 'Category not found' });
+      return;
     }
-
-    res.json(category);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve the category' });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-// POST: create a new category
 router.post('/', async (req, res) => {
   try {
     const newCategory = await Category.create(req.body);
-    res.status(201).json(newCategory);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to create a new category' });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-// PUT: update a category by its `id` value
 router.put('/:id', async (req, res) => {
   try {
     const updatedCategory = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
-
-    if (updatedCategory[0] === 0) {
-      return res.status(404).json({ error: 'Category not found' });
-    }
-
-    res.json({ message: 'Category updated successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to update the category' });
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-// DELETE: delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
     const deletedCategory = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
-
-    if (!deletedCategory) {
-      return res.status(404).json({ error: 'Category not found' });
-    }
-
-    res.json({ message: 'Category deleted successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to delete the category' });
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
+
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,98 +3,65 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-// GET all tags with associated Product data
 router.get('/', async (req, res) => {
   try {
     const tags = await Tag.findAll({
-      include: [
-        {
-          model: Product,
-          through: ProductTag,
-          as: 'products',
-        },
-      ],
+      include: [{ model: Product, through: ProductTag }],
     });
-    res.json(tags);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve tags' });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-// GET a single tag by its `id` with associated Product data
+
 router.get('/:id', async (req, res) => {
   try {
     const tag = await Tag.findByPk(req.params.id, {
-      include: [
-        {
-          model: Product,
-          through: ProductTag,
-          as: 'products',
-        },
-      ],
+      include: [{ model: Product, through: ProductTag }],
     });
-
     if (!tag) {
-      return res.status(404).json({ error: 'Tag not found' });
+      res.status(404).json({ message: 'Tag not found' });
+      return;
     }
-
-    res.json(tag);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve the tag' });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-// POST: create a new tag
+
 router.post('/', async (req, res) => {
   try {
     const newTag = await Tag.create(req.body);
-    res.status(201).json(newTag);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to create a new tag' });
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-// PUT: update a tag's name by its `id` value
+
 router.put('/:id', async (req, res) => {
   try {
     const updatedTag = await Tag.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
-
-    if (updatedTag[0] === 0) {
-      return res.status(404).json({ error: 'Tag not found' });
-    }
-
-    res.json({ message: 'Tag updated successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to update the tag' });
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-// DELETE: delete a tag by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
     const deletedTag = await Tag.destroy({
-      where: {
-        id: req.params.id,
-      },
+      where: { id: req.params.id },
     });
-
-    if (!deletedTag) {
-      return res.status(404).json({ error: 'Tag not found' });
-    }
-
-    res.json({ message: 'Tag deleted successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to delete the tag' });
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
+
 
 module.exports = router;
